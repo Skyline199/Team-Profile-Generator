83a132ec838db32b66e320ea0a635e44d3ed837b
unknown <aniljassi88@gmail.com>
2023-03-05 12:27:27 +0000

    added getRole command to extract data input

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -5,4 +5,17 @@ class Employee {
   this.id = id;
   this.email = email;
  }
+
+ getName () {
+    return this.name;
+ }
+ getId () {
+    return this.id;
+ }
+
+ getRole () {
+    return "Employee";
+ }
 }
+
+module.exports = Employee;
