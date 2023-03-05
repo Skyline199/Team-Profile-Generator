f62717ab34e93625afd6fdd191aef03eb6922511
unknown <aniljassi88@gmail.com>
2023-03-05 13:17:51 +0000

    added engineer class extension from employee class -  child class of employee class

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee');
+
+class Engineer extends Employee {
+     constructor (name, id, email, gitHubUser ){
+        //calling employee constructor here
+        super (name, id, email);
+        this.gitHubUser = gitHubUser;
+     }
+     }
+
+     GetgitHubUser  () {
+        return this.gitHubUser;
+
+     }
+     getRole() {
+        return "Engineer";
+     }
+     module.exports  = Engineer;
